5e7c78d31ad051c2319b6f8a47501fe0f691c3d1
unknown <danielerm1510@gmail.com>
2024-10-27 15:47:26 +0100

    patients app update1

--- patients/urls.py
+++ patients/urls.py
@@ -1,19 +1,11 @@
 from django.urls import path
 
 from .views import (
-    ListPatientView,
+    ListPatientsView,
     DetailPatientView,
-    ListInsuranceView,
-    DetailInsuranceView,
-    ListMedicalRecordView,
-    DetailMedicalRecordView,
 )
 
 urlpatterns = [
-    path('patients/', ListPatientView.as_view()),
+    path('patients/', ListPatientsView.as_view()),
     path('patients/<int:pk>/', DetailPatientView.as_view()),
-    path('insurances/', ListInsuranceView.as_view()),
-    path('insurances/<int:pk>/', DetailInsuranceView.as_view()),
-    path('medicalrecords/', ListMedicalRecordView.as_view()),
-    path('medicalrecords/<int:pk>/', DetailMedicalRecordView.as_view()),
 ]
