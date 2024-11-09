9ae60c7afd38ebf80395d90f7f920afa946375e9
unknown <danielerm1510@gmail.com>
2024-11-09 19:38:41 +0100

    viewsets update

--- doctors/viewsets.py
+++ doctors/viewsets.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .serializers import DoctorSerializer, DepartmentSerializer, DoctorAvailabilitySerializer, MedicalNoteSerializer
 from .models import Doctor, Department, DoctorAvailability, MedicalNote
 
@@ -7,6 +8,21 @@ from .models import Doctor, Department, DoctorAvailability, MedicalNote
 class DoctorViewSet(viewsets.ModelViewSet):
     serializer_class = DoctorSerializer
     queryset = Doctor.objects.all()
+
+    @action(['POST'], detail = True)
+    def set_on_vacation(self, requests, pk):
+        doctor = self.get_object()
+        doctor.is_on_vacation = True
+        doctor.save()
+        return Response({'status': 'On Vacation'})
+    
+    @action(['POST'], detail = True)
+    def set_off_vacation(self, requests, pk):
+        doctor = self.get_object()
+        doctor.is_on_vacation = False
+        doctor.save()
+        return Response({'status': 'On Dutty'})
+
 
 class DepartmentViewSet(viewsets.ModelViewSet):
     serializer_class = DepartmentSerializer
