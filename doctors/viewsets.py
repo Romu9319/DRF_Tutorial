63d7f982dc451a3aabdd18102bbfb481687ea109
unknown <danielerm1510@gmail.com>
2024-12-15 17:35:42 +0100

    testing nested endpoints

--- doctors/viewsets.py
+++ doctors/viewsets.py
@@ -1,6 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from bookings.serializers import AppointmentSerializer
+from bookings.models import Appointment
 from .serializers import DoctorSerializer, DepartmentSerializer, DoctorAvailabilitySerializer, MedicalNoteSerializer
 from .models import Doctor, Department, DoctorAvailability, MedicalNote
 
@@ -14,19 +16,35 @@ class DoctorViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsDoctor]
 
     @action(['POST'], detail=True, url_path='set-on-vacation')
-    def set_on_vacation(self, requests, pk):
+    def set_on_vacation(self, request, pk):
         doctor = self.get_object()
         doctor.is_on_vacation = True
         doctor.save()
         return Response({'status': 'On Vacation'})
     
     @action(['POST'], detail=True, url_path='set-off-vacation')
-    def set_off_vacation(self, requests, pk):
+    def set_off_vacation(self, request, pk):
         doctor = self.get_object() 
         doctor.is_on_vacation = False
         doctor.save()
         return Response({'status': 'On Dutty'})
+    
+    @action(['POST', 'GET'], detail=True, serializer_class=AppointmentSerializer)
+    def appointments(self, request, pk):
+        doctor = self.get_object()
 
+        if request.method == 'POST':
+            data = request.data.copy()
+            data['doctor'] = doctor.id  
+            serializer = AppointmentSerializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        if request.method == 'GET':
+            appointments = Appointment.objects.filter(doctor=doctor)
+            serializer = AppointmentSerializer(appointments, many=True)
+            return Response(serializer.data)
 
 class DepartmentViewSet(viewsets.ModelViewSet):
     serializer_class = DepartmentSerializer
